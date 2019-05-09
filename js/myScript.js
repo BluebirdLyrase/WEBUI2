50e89c0bff739d39dfd56006b5b47a146a3c60c3
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2019-05-09 22:09:59 +0700

    Finish Login

--- js/myScript.js
+++ js/myScript.js
@@ -41,4 +41,29 @@ $.get("data/orders.json", function(data,status){
     }else{
         console.log("BAD");}
 });
+//////////////////////////LOGIN//////////////////////////////////////////
+$(function () {
+    $('#mainform').submit(function (event) {
+    var form = $('#mainform')[0];
+    if (form.checkValidity() === false) {
+    event.preventDefault();
+    }
+    $(this).addClass('was-validated');
 
+    $("#go").click(function(){
+        var Email = $("#inputEmail").val();
+        var Password = $("#inputPassword").val();
+
+    if((Email=="admin@nw.com")&&(Password=="12345**")){
+            console.log("Login Successful")
+            return true
+    }else{
+        console.log("login Fail")
+        $("#warning").empty().append("In correct Email or Password");
+        return false
+    }
+
+    });
+    });
+
+});
