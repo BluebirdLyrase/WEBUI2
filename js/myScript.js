a802deb77e0d275b260264decf84af9492b95695
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2019-05-11 18:38:52 +0700

    Fix Login
    แก้ไขปุ่ม login ทำงานไม่ปกติ

--- js/myScript.js
+++ js/myScript.js
@@ -48,28 +48,30 @@ $.get("data/orders.json", function(data,status){
 
 //////////////////////////LOGIN//////////////////////////////////////////
 $(function () {
+    
+    $("#submit").click(function(){
+        var Email = $("#inputEmail").val();
+        var Password = $("#inputPassword").val();
+        $('#mainform').trigger('submit');
+        if((Email=="admin@nw.com")&&(Password=="12345**")){
+            console.log("Login Successful");
+            window.location.replace("dashboard.html")
+    }else{
+        console.log("login Fail")
+        $("#warning").empty().append("In correct Email or Password");
+    }
+
     $('#mainform').submit(function (event) {
     var form = $('#mainform')[0];
     if (form.checkValidity() === false) {
     event.preventDefault();
     }
     $(this).addClass('was-validated');
-
-    $("#go").click(function(){
-        var Email = $("#inputEmail").val();
-        var Password = $("#inputPassword").val();
-
-    if((Email=="admin@nw.com")&&(Password=="12345**")){
-            console.log("Login Successful")
-            return true
-    }else{
-        console.log("login Fail")
-        $("#warning").empty().append("In correct Email or Password");
-        return false
-    }
+    });
 
     });
-    });
+
+
 
 });
 
